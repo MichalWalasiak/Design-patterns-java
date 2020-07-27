6f5248e450fd1648e8d0cdcc31d0a707a9de09bd
Michał Walasiak <m.walasiak@gmail.com>
2020-07-27 17:52:49 +0200

    Add class TextMessage

---
+++ src/main/java/io/github/mwalasiak/Observer/notification/TextMessage.java
@@ -0,0 +1,9 @@
+package io.github.mwalasiak.Observer.notification;
+
+import io.github.mwalasiak.Observer.Order.Order;
+
+public class TextMessage {
+    public void updateOrderStatus(Order order) {
+        System.out.println("order nr " + order.getOrderNumber() + "has changed status to " + order.getOrderStatus());
+    }
+}
