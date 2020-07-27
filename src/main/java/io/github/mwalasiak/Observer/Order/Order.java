6816e902048ceefa37c1a426913ac0ed95785b94
Michał Walasiak <m.walasiak@gmail.com>
2020-07-27 21:21:08 +0200

    Initialize Set of Observers

--- src/main/java/io/github/mwalasiak/Observer/Order/Order.java
+++ src/main/java/io/github/mwalasiak/Observer/Order/Order.java
@@ -1,13 +1,34 @@
 package io.github.mwalasiak.Observer.Order;
 
-public class Order {
+import io.github.mwalasiak.Observer.notification.Observer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Order implements Observable {
 
     private Long orderNumber;
     private OrderStatus orderStatus;
+    private Set<Observer> registeredObservers = new HashSet<>();
 
     public Order(final Long orderNumber, final OrderStatus orderStatus) {
         this.orderNumber = orderNumber;
         this.orderStatus = orderStatus;
+    }
+
+    @Override
+    public void registerObserver(final Observer observer) {
+
+    }
+
+    @Override
+    public void unRegisterObserver(final Observer observer) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
     }
 
     public Long getOrderNumber() {
