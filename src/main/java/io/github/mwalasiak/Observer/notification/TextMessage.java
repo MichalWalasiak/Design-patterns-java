0986249deb071876d6daf18916b5200bddeb014e
Michał Walasiak <m.walasiak@gmail.com>
2020-07-27 21:13:15 +0200

    Change existing methods into overriden

--- src/main/java/io/github/mwalasiak/Observer/notification/TextMessage.java
+++ src/main/java/io/github/mwalasiak/Observer/notification/TextMessage.java
@@ -3,12 +3,8 @@ package io.github.mwalasiak.Observer.notification;
 import io.github.mwalasiak.Observer.Order.Order;
 
 public class TextMessage implements Observer {
-    public void updateOrderStatus(Order order) {
+
+    public void update(Order order) {
         System.out.println("Sms - order nr " + order.getOrderNumber() + " has changed status to " + order.getOrderStatus());
-    }
-
-    @Override
-    public void update(final Order order) {
-
     }
 }
