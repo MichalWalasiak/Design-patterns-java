e8d6c0117ab710423a383316b1c1601d8075fdfd
Michał Walasiak <m.walasiak@gmail.com>
2020-07-27 19:00:33 +0200

    Set data to implement observer pattern

--- src/main/java/io/github/mwalasiak/Main.java
+++ src/main/java/io/github/mwalasiak/Main.java
@@ -1,8 +1,23 @@
 package io.github.mwalasiak;
 
+import io.github.mwalasiak.Observer.Order.Order;
+import io.github.mwalasiak.Observer.Order.OrderStatus;
+import io.github.mwalasiak.Observer.notification.Email;
+import io.github.mwalasiak.Observer.notification.MobileApp;
+import io.github.mwalasiak.Observer.notification.TextMessage;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Order order = new Order(110L, OrderStatus.Registered);
+
+        TextMessage textMessage = new TextMessage();
+        MobileApp mobileApp = new MobileApp();
+        Email email = new Email();
+
+        textMessage.updateOrderStatus(order);
+        mobileApp.updateOrderStatus(order);
+        email.updateOrderStatus(order);
     }
 }
