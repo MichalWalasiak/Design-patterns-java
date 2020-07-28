cd991d1440c5c4988022d6ddd46875c5d415dfa9
Michał Walasiak <m.walasiak@gmail.com>
2020-07-28 14:20:03 +0200

    Add field, constructor and method

--- src/main/java/io/github/mwalasiak/Singleton/GameEngine.java
+++ src/main/java/io/github/mwalasiak/Singleton/GameEngine.java
@@ -5,11 +5,23 @@ public class GameEngine {
     private int healthPoints = 100;
     private String characterName = "";
 
+    private GameEngine instance;
+
+    public GameEngine() {
+    }
+
     public void run() {
         while (true){
             // waits for player input
             // change game status
             // render graphics
         }
+    }
+
+    public GameEngine getInstance() {
+        if (instance == null){
+            instance = new GameEngine();
+        }
+        return instance;
     }
 }
