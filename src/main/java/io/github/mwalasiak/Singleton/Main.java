2d96f8ce4e1e8a914f1d6a2f512caf6eac602d70
Michał Walasiak <m.walasiak@gmail.com>
2020-07-28 14:35:24 +0200

    Check if only one instance exists

--- src/main/java/io/github/mwalasiak/Singleton/Main.java
+++ src/main/java/io/github/mwalasiak/Singleton/Main.java
@@ -1,4 +1,13 @@
 package io.github.mwalasiak.Singleton;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        GameEngine engine = GameEngine.getInstance();
+
+        GameEngine engine1 = GameEngine.getInstance();
+
+        System.out.println(engine == engine1);
+    }
 }
