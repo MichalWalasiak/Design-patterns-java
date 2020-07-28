bd437757e869688ac29f9b7ca2e5fb48dfe8ef19
Michał Walasiak <m.walasiak@gmail.com>
2020-07-28 18:16:44 +0200

    Updates in GameGuess class

--- src/main/java/io/github/mwalasiak/Singleton/practice/GameGuess.java
+++ src/main/java/io/github/mwalasiak/Singleton/practice/GameGuess.java
@@ -1,28 +1,38 @@
 package io.github.mwalasiak.Singleton.practice;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GameGuess {
+    private int score = 0;
+    private Random random = new Random();
 
     Scanner scanner = new Scanner(System.in);
-    private int playerGuess;
-    private double randomNumber;
-    private int score = 0;
     private static GameGuess instance;
 
     private GameGuess() {
     }
 
+    public Object readResolve() {
+        return getInstance();
+    }
+
     public void play() {
         for (int i = 0; i < 10; i++) {
-            playerGuess = scanner.nextInt();
-            randomNumber = Math.random() * 10;
+            final int playerGuess = scanner.nextInt();
+            final int randomNumber = random.nextInt(9);
 
             if (playerGuess == randomNumber) {
                 score++;
+                System.out.println("Hit");
             }
+            System.out.println(randomNumber);
+            System.out.println("your score is " + score);
         }
+    }
 
+    public int getScore() {
+        return score;
     }
 
     public static GameGuess getInstance() {
@@ -35,9 +45,5 @@ public class GameGuess {
             }
         }
         return instance;
-    }
-
-    public Object readResolve() {
-        return getInstance();
     }
 }
