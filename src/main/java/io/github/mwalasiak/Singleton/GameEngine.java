6ba9a9f1287c5f0ef560ec5e500f2df069871b8b
Michał Walasiak <m.walasiak@gmail.com>
2020-07-28 14:15:11 +0200

    Implement sample method

--- src/main/java/io/github/mwalasiak/Singleton/GameEngine.java
+++ src/main/java/io/github/mwalasiak/Singleton/GameEngine.java
@@ -4,4 +4,12 @@ public class GameEngine {
 
     private int healthPoints = 100;
     private String characterName = "";
+
+    public void run() {
+        while (true){
+            // waits for player input
+            // change game status
+            // render graphics
+        }
+    }
 }
