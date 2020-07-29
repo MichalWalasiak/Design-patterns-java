5facbefbc3685686d56315f40e6a04c83c089b93
Michał Walasiak <m.walasiak@gmail.com>
2020-07-29 21:24:18 +0200

    Create Hause class with gets and sets

---
+++ src/main/java/io/github/mwalasiak/Builder/house/Hause.java
@@ -0,0 +1,59 @@
+package io.github.mwalasiak.Builder.house;
+
+public class Hause {
+
+    private String walls;
+    private String floors;
+    private String roof;
+    private String windows;
+    private String doors;
+    private String garage;
+
+    public String getWalls() {
+        return walls;
+    }
+
+    public void setWalls(final String walls) {
+        this.walls = walls;
+    }
+
+    public String getFloors() {
+        return floors;
+    }
+
+    public void setFloors(final String floors) {
+        this.floors = floors;
+    }
+
+    public String getRoof() {
+        return roof;
+    }
+
+    public void setRoof(final String roof) {
+        this.roof = roof;
+    }
+
+    public String getWindows() {
+        return windows;
+    }
+
+    public void setWindows(final String windows) {
+        this.windows = windows;
+    }
+
+    public String getDoors() {
+        return doors;
+    }
+
+    public void setDoors(final String doors) {
+        this.doors = doors;
+    }
+
+    public String getGarage() {
+        return garage;
+    }
+
+    public void setGarage(final String garage) {
+        this.garage = garage;
+    }
+}
