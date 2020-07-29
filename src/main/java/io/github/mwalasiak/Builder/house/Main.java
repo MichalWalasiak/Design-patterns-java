1e2a86d833e1d9a52c1b573048ed271f825ba206
Michał Walasiak <m.walasiak@gmail.com>
2020-07-29 21:42:46 +0200

    Initialize first object in Main class

--- src/main/java/io/github/mwalasiak/Builder/house/Main.java
+++ src/main/java/io/github/mwalasiak/Builder/house/Main.java
@@ -1,4 +1,10 @@
 package io.github.mwalasiak.Builder.house;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Hause hause1 = new Hause("walls", "floors", "rooms", "roof", "windows", "doors", "garage");
+
+    }
 }
