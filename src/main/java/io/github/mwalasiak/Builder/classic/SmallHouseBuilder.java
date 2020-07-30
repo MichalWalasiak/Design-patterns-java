855c19148088e7c58bcbc158bae390f90e2b303d
Michał Walasiak <m.walasiak@gmail.com>
2020-07-30 14:40:52 +0200

    Override methods with small values

--- src/main/java/io/github/mwalasiak/Builder/classic/SmallHouseBuilder.java
+++ src/main/java/io/github/mwalasiak/Builder/classic/SmallHouseBuilder.java
@@ -10,37 +10,37 @@ public class SmallHouseBuilder implements HouseBuilder {
 
     @Override
     public void buildWalls() {
-
+        this.house.setWalls("small walls");
     }
 
     @Override
     public void buildFloors() {
-
+        this.house.setFloors("small floors");
     }
 
     @Override
     public void buildRooms() {
-
+        this.house.setRooms("small rooms");
     }
 
     @Override
     public void buildRoof() {
-
+        this.house.setRoof("small roof");
     }
 
     @Override
     public void buildWindows() {
-
+        this.house.setWindows("small window");
     }
 
     @Override
     public void buildDoors() {
-
+        this.house.setDoors("small doors");
     }
 
     @Override
     public void buildGarage() {
-
+        this.house.setGarage("small garage");
     }
 
     @Override
