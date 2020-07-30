3525d37ab7d1273213dda93f6272ab7f6c7c2640
Michał Walasiak <m.walasiak@gmail.com>
2020-07-30 14:13:20 +0200

    Add interface with methods

---
+++ src/main/java/io/github/mwalasiak/Builder/classic/HouseBuilder.java
@@ -0,0 +1,12 @@
+package io.github.mwalasiak.Builder.classic;
+
+public interface HouseBuilder {
+
+    void buildWalls();
+    void buildFloors();
+    void buildRooms();
+    void buildRoof();
+    void buildWindows();
+    void buildDoors();
+    void buildGarage();
+}
