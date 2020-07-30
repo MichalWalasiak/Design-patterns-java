74b7483f83363c72c01fed43a1fe964c35f45af8
Michał Walasiak <m.walasiak@gmail.com>
2020-07-30 14:44:55 +0200

    Add class BigHouseBuilder

---
+++ src/main/java/io/github/mwalasiak/Builder/classic/BigHouseBuilder.java
@@ -0,0 +1,10 @@
+package io.github.mwalasiak.Builder.classic;
+
+public class BigHouseBuilder implements HouseBuilder {
+
+    private House house;
+
+    public BigHouseBuilder() {
+        this.house = new House();
+    }
+}
