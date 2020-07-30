4d2e64fa2d86e3d6ae460fba43c524561e649ba6
Michał Walasiak <m.walasiak@gmail.com>
2020-07-30 14:20:28 +0200

    Create Director class with constructor

---
+++ src/main/java/io/github/mwalasiak/Builder/classic/HouseDirector.java
@@ -0,0 +1,10 @@
+package io.github.mwalasiak.Builder.classic;
+
+public class HouseDirector {
+
+    private HouseBuilder houseBuilder;
+
+    public HouseDirector(final HouseBuilder houseBuilder) {
+        this.houseBuilder = houseBuilder;
+    }
+}
