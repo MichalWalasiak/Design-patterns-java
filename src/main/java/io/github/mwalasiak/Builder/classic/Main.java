9c587eba87e8c16fb1e769921949dec85ad820aa
Michał Walasiak <m.walasiak@gmail.com>
2020-07-30 17:29:39 +0200

    Add private fields and methods

--- src/main/java/io/github/mwalasiak/Builder/classic/Main.java
+++ src/main/java/io/github/mwalasiak/Builder/classic/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SmallHouseBuilder smallHouseBuilder = new SmallHouseBuilder();
+        BigHouseBuilder bigHouseBuilder = new BigHouseBuilder();
+
+        HouseDirector smallHouseDirector = new HouseDirector(smallHouseBuilder);
+        smallHouseDirector.buildHouse();
+
+        HouseDirector bigHouseDirector = new HouseDirector(bigHouseBuilder);
+        bigHouseDirector.buildHouse();
+
+
     }
 }
