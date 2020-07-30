2ead05b80688295a00d88115f7791f6525c25632
Michał Walasiak <m.walasiak@gmail.com>
2020-07-30 14:34:55 +0200

    Create class SmallHouseBuilder

---
+++ src/main/java/io/github/mwalasiak/Builder/classic/SmallHouseBuilder.java
@@ -0,0 +1,50 @@
+package io.github.mwalasiak.Builder.classic;
+
+public class SmallHouseBuilder implements HouseBuilder {
+
+    House house;
+
+    public SmallHouseBuilder() {
+        this.house = new House();
+    }
+
+    @Override
+    public void buildWalls() {
+
+    }
+
+    @Override
+    public void buildFloors() {
+
+    }
+
+    @Override
+    public void buildRooms() {
+
+    }
+
+    @Override
+    public void buildRoof() {
+
+    }
+
+    @Override
+    public void buildWindows() {
+
+    }
+
+    @Override
+    public void buildDoors() {
+
+    }
+
+    @Override
+    public void buildGarage() {
+
+    }
+
+    @Override
+    public House getHouse() {
+        return null;
+    }
+}
