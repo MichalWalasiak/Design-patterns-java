5a26b577ae1c95f66f067ac5067899f58ad18b9a
Michał Walasiak <m.walasiak@gmail.com>
2020-07-31 15:14:12 +0200

    Add constructor

--- src/main/java/io/github/mwalasiak/Builder/practice/FlightLeg.java
+++ src/main/java/io/github/mwalasiak/Builder/practice/FlightLeg.java
@@ -7,6 +7,14 @@ public class FlightLeg {
     private boolean delayed;
     private int price;
 
+    private FlightLeg(FlightLegBuilder flightLegBuilder) {
+        from = flightLegBuilder.from;
+        to = flightLegBuilder.to;
+        flightLegBuilder.setDelayed(true);
+        price = flightLegBuilder.price;
+
+    }
+
     public String getFrom() {
         return from;
     }
@@ -23,6 +31,10 @@ public class FlightLeg {
         return price;
     }
 
+    public void setDelayed(final boolean delayed) {
+        this.delayed = delayed;
+    }
+
     @Override
     public String toString() {
         return "FlightLeg{" +
@@ -34,10 +46,19 @@ public class FlightLeg {
     }
 
     public static class FlightLegBuilder{
+
         private String from;
         private String to;
         private boolean delayed;
         private int price;
+
+        public void setDelayed(final boolean delayed) {
+            this.delayed = delayed;
+        }
+
+        public FlightLeg build () {
+            return new FlightLeg(this);
+        }
 
     }
 }
