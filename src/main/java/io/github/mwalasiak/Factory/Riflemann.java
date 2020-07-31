21115a541bf10acf8c74b2f696a83b9738e78634
Michał Walasiak <m.walasiak@gmail.com>
2020-07-31 16:48:15 +0200

    Add constructor

--- src/main/java/io/github/mwalasiak/Factory/Riflemann.java
+++ src/main/java/io/github/mwalasiak/Factory/Riflemann.java
@@ -1,4 +1,7 @@
 package io.github.mwalasiak.Factory;
 
 public class Riflemann extends Unit {
+    public Riflemann(final int healthPoints, final int experience, final int damageDone) {
+        super(healthPoints, experience, damageDone);
+    }
 }
