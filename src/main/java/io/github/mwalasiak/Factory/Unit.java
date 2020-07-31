7a9360135673bfed0e9d66320675e5f32607ec4b
Michał Walasiak <m.walasiak@gmail.com>
2020-07-31 16:22:50 +0200

    Create new abstract class

---
+++ src/main/java/io/github/mwalasiak/Factory/Unit.java
@@ -0,0 +1,16 @@
+package io.github.mwalasiak.Factory;
+
+public class Unit {
+
+    private int healthPoints;
+    private int experience;
+    private int damageDone;
+
+    public Unit(final int healthPoints, final int experience, final int damageDone) {
+        this.healthPoints = healthPoints;
+        this.experience = experience;
+        this.damageDone = damageDone;
+    }
+
+
+}
