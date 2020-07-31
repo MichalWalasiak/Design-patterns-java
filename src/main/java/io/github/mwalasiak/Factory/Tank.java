e3a8a67870b8cedb3bda436ab4c5486457417638
Michał Walasiak <m.walasiak@gmail.com>
2020-07-31 16:48:25 +0200

    Add constructor

--- src/main/java/io/github/mwalasiak/Factory/Tank.java
+++ src/main/java/io/github/mwalasiak/Factory/Tank.java
@@ -1,4 +1,7 @@
 package io.github.mwalasiak.Factory;
 
 public class Tank extends Unit {
+    public Tank(final int healthPoints, final int experience, final int damageDone) {
+        super(healthPoints, experience, damageDone);
+    }
 }
