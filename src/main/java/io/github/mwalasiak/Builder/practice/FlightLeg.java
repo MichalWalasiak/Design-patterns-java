a3e8d4172ae0b5689c9eaa086c39831631214415
Michał Walasiak <m.walasiak@gmail.com>
2020-07-31 14:59:25 +0200

    Create class to practice builder pattern

---
+++ src/main/java/io/github/mwalasiak/Builder/practice/FlightLeg.java
@@ -0,0 +1,11 @@
+package io.github.mwalasiak.Builder.practice;
+
+public class FlightLeg {
+
+    private String from;
+    private String to;
+    private boolean delayed;
+    private int price;
+
+
+}
