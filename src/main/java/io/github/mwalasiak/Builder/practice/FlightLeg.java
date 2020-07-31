a302015a75d089c066e0e4f39f0f435b242b1a6d
Michał Walasiak <m.walasiak@gmail.com>
2020-07-31 15:01:04 +0200

    Add getters and toString method

--- src/main/java/io/github/mwalasiak/Builder/practice/FlightLeg.java
+++ src/main/java/io/github/mwalasiak/Builder/practice/FlightLeg.java
@@ -7,5 +7,29 @@ public class FlightLeg {
     private boolean delayed;
     private int price;
 
+    public String getFrom() {
+        return from;
+    }
 
+    public String getTo() {
+        return to;
+    }
+
+    public boolean isDelayed() {
+        return delayed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightLeg{" +
+                "from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", delayed=" + delayed +
+                ", price=" + price +
+                '}';
+    }
 }
