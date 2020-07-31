1b2fed29da611b97883ed161405fc7f548b50ef5
Michał Walasiak <m.walasiak@gmail.com>
2020-07-31 16:48:43 +0200

    Add switch statement

--- src/main/java/io/github/mwalasiak/Factory/UnitFactory.java
+++ src/main/java/io/github/mwalasiak/Factory/UnitFactory.java
@@ -1,8 +1,18 @@
 package io.github.mwalasiak.Factory;
 
+import javax.lang.model.type.TypeKind;
+
 public class UnitFactory extends Factory {
     @Override
     public Unit createUnit(UnitType type) {
-        return null;
+
+        switch (type) {
+            case TANK:
+                return new Tank(200, 0, 20);
+            case RIFLEMANN:
+                return new Riflemann(100, 10, 5);
+            default:
+                throw new UnsupportedOperationException("no such type");
+        }
     }
 }
