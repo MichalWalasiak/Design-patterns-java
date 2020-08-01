e476f09660dec693c74e267f4c5567e997f8700e
Michał Walasiak <m.walasiak@gmail.com>
2020-08-01 10:54:03 +0200

    Change access modifier

--- src/main/java/io/github/mwalasiak/Factory/Tank.java
+++
@@ -1,7 +0,0 @@
-package io.github.mwalasiak.Factory;
-
-public class Tank extends Unit {
-    public Tank(final int healthPoints, final int experience, final int damageDone) {
-        super(healthPoints, experience, damageDone);
-    }
-}
---
+++ src/main/java/io/github/mwalasiak/Factory/unit/Tank.java
@@ -0,0 +1,7 @@
+package io.github.mwalasiak.Factory.unit;
+
+public class Tank extends Unit {
+    Tank(final int healthPoints, final int experience, final int damageDone) {
+        super(healthPoints, experience, damageDone);
+    }
+}
