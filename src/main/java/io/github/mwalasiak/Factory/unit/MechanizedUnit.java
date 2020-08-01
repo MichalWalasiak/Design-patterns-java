faac1def8f2f97062b329cfc34fd2da09630c90e
Michał Walasiak <m.walasiak@gmail.com>
2020-08-01 11:06:42 +0200

    Insert data into class

--- src/main/java/io/github/mwalasiak/Factory/unit/MechanizedUnit.java
+++ src/main/java/io/github/mwalasiak/Factory/unit/MechanizedUnit.java
@@ -1,4 +1,26 @@
 package io.github.mwalasiak.Factory.unit;
 
 public class MechanizedUnit {
+
+    private int healthPoints;
+    private int experience;
+    private int damageDone;
+
+    protected MechanizedUnit(final int healthPoints, final int experience, final int damageDone) {
+        this.healthPoints = healthPoints;
+        this.experience = experience;
+        this.damageDone = damageDone;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getDamageDone() {
+        return damageDone;
+    }
 }
