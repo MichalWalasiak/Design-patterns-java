c3cc8b66b867fc71bb207796f7a69340d2ee24cd
Michał Walasiak <m.walasiak@gmail.com>
2020-08-01 11:02:21 +0200

    Updates due to class name change

--- src/main/java/io/github/mwalasiak/Factory/unit/Riflemann.java
+++ src/main/java/io/github/mwalasiak/Factory/unit/Riflemann.java
@@ -1,6 +1,6 @@
 package io.github.mwalasiak.Factory.unit;
 
-public class Riflemann extends Unit {
+public class Riflemann extends InfantryUnit {
     Riflemann(final int healthPoints, final int experience, final int damageDone) {
         super(healthPoints, experience, damageDone);
     }
