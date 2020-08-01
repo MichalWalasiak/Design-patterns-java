d8aeb571ed3034ee16e91fb2f4285508d07b4b54
Michał Walasiak <m.walasiak@gmail.com>
2020-08-01 10:53:53 +0200

    Change access modifier

--- src/main/java/io/github/mwalasiak/Factory/Riflemann.java
+++
@@ -1,7 +0,0 @@
-package io.github.mwalasiak.Factory;
-
-public class Riflemann extends Unit {
-    public Riflemann(final int healthPoints, final int experience, final int damageDone) {
-        super(healthPoints, experience, damageDone);
-    }
-}
---
+++ src/main/java/io/github/mwalasiak/Factory/unit/Riflemann.java
@@ -0,0 +1,7 @@
+package io.github.mwalasiak.Factory.unit;
+
+public class Riflemann extends Unit {
+    Riflemann(final int healthPoints, final int experience, final int damageDone) {
+        super(healthPoints, experience, damageDone);
+    }
+}
