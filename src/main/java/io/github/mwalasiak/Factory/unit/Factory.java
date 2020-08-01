a41f3c0ad3e97a716a3925cc27620028448aefab
Michał Walasiak <m.walasiak@gmail.com>
2020-08-01 11:00:49 +0200

    Updates due to class name change

--- src/main/java/io/github/mwalasiak/Factory/unit/Factory.java
+++ src/main/java/io/github/mwalasiak/Factory/unit/Factory.java
@@ -2,5 +2,5 @@ package io.github.mwalasiak.Factory.unit;
 
 public abstract class Factory {
 
-    public abstract Unit createUnit(UnitType type);
+    public abstract InfantryUnit createUnit(UnitType type);
 }
