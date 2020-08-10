62f014828446a5203eb0457b5065c1f7215f36d7
Michał Walasiak <m.walasiak@gmail.com>
2020-08-10 10:02:34 +0200

    Create new class

---
+++ src/main/java/io/github/mwalasiak/Factory/unit/UnitMechanizedFactory.java
@@ -0,0 +1,8 @@
+package io.github.mwalasiak.Factory.unit;
+
+public class UnitMechanizedFactory extends MechanizedFactory {
+    @Override
+    public MechanizedUnit createUnit(final UnitType type) {
+        return null;
+    }
+}
