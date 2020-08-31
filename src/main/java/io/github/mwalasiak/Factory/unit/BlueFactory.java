198a7ef07cefc2df0d6da2585aeb22a02378110f
Michał Walasiak <m.walasiak@gmail.com>
2020-08-31 14:26:14 +0200

    Add new class

---
+++ src/main/java/io/github/mwalasiak/Factory/unit/BlueFactory.java
@@ -0,0 +1,14 @@
+package io.github.mwalasiak.Factory.unit;
+
+public class BlueFactory extends Factory {
+
+    @Override
+    public InfantryUnit createInfantryUnit(final UnitType type) {
+        return null;
+    }
+
+    @Override
+    public MechanizedUnit createMechanizedUnit(final UnitType type) {
+        return null;
+    }
+}
