bfcd3b7ff9bbe53815c6d73cf5e3e11be65bf0a5
Michał Walasiak <m.walasiak@gmail.com>
2020-08-31 15:33:13 +0200

    Create new class

---
+++ src/main/java/io/github/mwalasiak/Factory/unit/FighterJet.java
@@ -0,0 +1,8 @@
+package io.github.mwalasiak.Factory.unit;
+
+public class FighterJet extends AirUnit {
+
+    FighterJet(final int healthPoints, final int experience, final int damageDone) {
+        super(healthPoints, experience, damageDone);
+    }
+}
