b6d4e556a4dab62a037790779d91e09f0e98033f
Michał Walasiak <m.walasiak@gmail.com>
2020-08-31 13:13:25 +0200

    Updates due to class name change

--- src/main/java/io/github/mwalasiak/Factory/Main.java
+++ src/main/java/io/github/mwalasiak/Factory/Main.java
@@ -2,14 +2,14 @@ package io.github.mwalasiak.Factory;
 
 import io.github.mwalasiak.Factory.unit.InfantryFactory;
 import io.github.mwalasiak.Factory.unit.InfantryUnit;
-import io.github.mwalasiak.Factory.unit.UnitInfantryFactory;
+import io.github.mwalasiak.Factory.unit.RedUnitInfantryFactory;
 import io.github.mwalasiak.Factory.unit.UnitType;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        InfantryFactory infantryFactory = new UnitInfantryFactory();
+        InfantryFactory infantryFactory = new RedUnitInfantryFactory();
         InfantryUnit tank = infantryFactory.createUnit(UnitType.TANK);
         InfantryUnit riflemann = infantryFactory.createUnit(UnitType.RIFLEMANN);
     }
