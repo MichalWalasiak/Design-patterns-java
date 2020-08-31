821e65ae2e7ba6089c71c824cc1cdbb1d36149da
Michał Walasiak <m.walasiak@gmail.com>
2020-08-31 15:21:36 +0200

    Add abstract method to class

--- src/main/java/io/github/mwalasiak/Factory/unit/Factory.java
+++ src/main/java/io/github/mwalasiak/Factory/unit/Factory.java
@@ -4,4 +4,6 @@ public abstract class Factory {
 
     public abstract InfantryUnit createInfantryUnit(UnitType type);
     public abstract MechanizedUnit createMechanizedUnit(UnitType type);
+    public abstract AirUnit createAirUnit(UnitType type);
+
 }
