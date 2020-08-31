a71fa893e54ad119955235abff263159f3a64a36
Michał Walasiak <m.walasiak@gmail.com>
2020-08-31 14:26:23 +0200

    Add new class

---
+++ src/main/java/io/github/mwalasiak/Factory/unit/RedFactory.java
@@ -0,0 +1,13 @@
+package io.github.mwalasiak.Factory.unit;
+
+public class RedFactory extends Factory {
+    @Override
+    public InfantryUnit createInfantryUnit(final UnitType type) {
+        return null;
+    }
+
+    @Override
+    public MechanizedUnit createMechanizedUnit(final UnitType type) {
+        return null;
+    }
+}
