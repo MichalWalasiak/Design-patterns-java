e42b72c485f491d0d2ff5cae12f05d509c2d878b
Michał Walasiak <m.walasiak@gmail.com>
2020-08-31 14:13:23 +0200

    Change class name

--- src/main/java/io/github/mwalasiak/Factory/unit/Factory.java
+++ src/main/java/io/github/mwalasiak/Factory/unit/Factory.java
@@ -1,6 +1,6 @@
 package io.github.mwalasiak.Factory.unit;
 
-public abstract class InfantryFactory {
+public abstract class Factory {
 
     public abstract InfantryUnit createUnit(UnitType type);
 }
