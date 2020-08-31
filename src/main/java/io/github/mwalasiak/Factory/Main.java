1f5eaa653f03b68d43ad5d7704f2a6df216c431f
Michał Walasiak <m.walasiak@gmail.com>
2020-08-31 15:09:37 +0200

    Add sample fields to Main class

--- src/main/java/io/github/mwalasiak/Factory/Main.java
+++ src/main/java/io/github/mwalasiak/Factory/Main.java
@@ -1,9 +1,18 @@
 package io.github.mwalasiak.Factory;
 
+import io.github.mwalasiak.Factory.unit.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        BlueFactory blueFactory = new BlueFactory();
+        RedFactory redFactory = new RedFactory();
 
+        InfantryUnit blueRiflemann = blueFactory.createInfantryUnit(UnitType.RIFLEMANN);
+        MechanizedUnit blueTank = blueFactory.createMechanizedUnit(UnitType.TANK);
+
+        InfantryUnit redRiflemann = redFactory.createInfantryUnit(UnitType.RIFLEMANN);
+        MechanizedUnit redTank = redFactory.createMechanizedUnit(UnitType.TANK);
     }
 }
