359f6c756a5bdae495b9c5e8d765e634cddf7d82
Michał Walasiak <m.walasiak@gmail.com>
2020-08-31 14:38:26 +0200

    Update extended class

--- src/main/java/io/github/mwalasiak/Factory/unit/Tank.java
+++ src/main/java/io/github/mwalasiak/Factory/unit/Tank.java
@@ -1,6 +1,6 @@
 package io.github.mwalasiak.Factory.unit;
 
-public class Tank extends InfantryUnit {
+public class Tank extends MechanizedUnit {
     Tank(final int healthPoints, final int experience, final int damageDone) {
         super(healthPoints, experience, damageDone);
     }
