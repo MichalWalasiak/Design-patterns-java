d2aacd1d0bd50d5fca8b340384476b49b4e4d7fa
Michał Walasiak <m.walasiak@gmail.com>
2020-09-07 21:52:05 +0200

    create class with method

---
+++ src/main/java/io/github/mwalasiak/Factory/training/Ford.java
@@ -0,0 +1,8 @@
+package io.github.mwalasiak.Factory.training;
+
+public class Ford extends Car {
+
+    public Ford(final String engineCapacity, final String typeOfFuel, final String productionYear, final String steringWheelPosition) {
+        super(engineCapacity, typeOfFuel, productionYear, steringWheelPosition);
+    }
+}
