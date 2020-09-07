dceed9168b5adbe4b8eb07ec016a1c6d2d248bbb
Michał Walasiak <m.walasiak@gmail.com>
2020-09-07 21:51:31 +0200

    fix spelling bug

--- src/main/java/io/github/mwalasiak/Factory/training/Audi.java
+++ src/main/java/io/github/mwalasiak/Factory/training/Audi.java
@@ -2,7 +2,7 @@ package io.github.mwalasiak.Factory.training;
 
 public class Audi extends Car {
 
-    ublic Audi(final String engineCapacity, final String typeOfFuel, final String productionYear, final String steringWheelPosition) {
+    public  Audi(final String engineCapacity, final String typeOfFuel, final String productionYear, final String steringWheelPosition) {
         super(engineCapacity, typeOfFuel, productionYear, steringWheelPosition);
     }
 }
