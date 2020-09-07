6d895b9a9deb082a7b18dfbe569ac6fc686e4e54
Michał Walasiak <m.walasiak@gmail.com>
2020-09-07 21:45:46 +0200

    change field type

--- src/main/java/io/github/mwalasiak/Factory/training/Car.java
+++ src/main/java/io/github/mwalasiak/Factory/training/Car.java
@@ -5,10 +5,16 @@ public class Car {
     private String engineCapacity;
     private String typeOfFuel;
     private String productionYear;
+    private SteringWheelPosition position;
 
-    public Car(final String engineCapacity, final String typeOfFuel, final String productionYear) {
+    protected Car(final String engineCapacity, final String typeOfFuel, final String productionYear, final String steringWheelPosition) {
         this.engineCapacity = engineCapacity;
         this.typeOfFuel = typeOfFuel;
         this.productionYear = productionYear;
+        this.steringWheelPosition = steringWheelPosition;
+    }
+
+    public SteringWheelPosition getPosition() {
+        return position;
     }
 }
