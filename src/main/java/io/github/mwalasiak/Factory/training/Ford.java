22cb96cf983ab3b072b3c5f78852cfd442a604f2
Michał Walasiak <m.walasiak@gmail.com>
2020-09-09 22:31:46 +0200

    change parameter type

--- src/main/java/io/github/mwalasiak/Factory/training/Ford.java
+++ src/main/java/io/github/mwalasiak/Factory/training/Ford.java
@@ -2,7 +2,7 @@ package io.github.mwalasiak.Factory.training;
 
 public class Ford extends Car {
 
-    public Ford(final String engineCapacity, final String typeOfFuel, final String productionYear, final String steringWheelPosition) {
-        super(engineCapacity, typeOfFuel, productionYear, steringWheelPosition);
+    public Ford(final String engineCapacity, final String typeOfFuel, final String productionYear, final SteringWheelPosition position) {
+        super(engineCapacity, typeOfFuel, productionYear, position);
     }
 }
