0e0ba0dcc1beab15f4d7add290359829795506b0
Michał Walasiak <m.walasiak@gmail.com>
2020-09-09 22:30:49 +0200

    change parameter type

--- src/main/java/io/github/mwalasiak/Factory/training/Audi.java
+++ src/main/java/io/github/mwalasiak/Factory/training/Audi.java
@@ -2,7 +2,7 @@ package io.github.mwalasiak.Factory.training;
 
 public class Audi extends Car {
 
-    public  Audi(final String engineCapacity, final String typeOfFuel, final String productionYear, final String steringWheelPosition) {
-        super(engineCapacity, typeOfFuel, productionYear, steringWheelPosition);
+    public  Audi(final String engineCapacity, final String typeOfFuel, final String productionYear, final SteringWheelPosition position) {
+        super(engineCapacity, typeOfFuel, productionYear, position);
     }
 }
