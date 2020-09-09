0f44f596d0b36084ee0a4eb730d03020c5dfbb35
Michał Walasiak <m.walasiak@gmail.com>
2020-09-09 22:31:38 +0200

    implement buildFord method

---
+++ src/main/java/io/github/mwalasiak/Factory/training/ContinentalFactory.java
@@ -0,0 +1,22 @@
+package io.github.mwalasiak.Factory.training;
+
+public class ContinentalFactory implements Factory {
+
+    SteringWheelPosition position = SteringWheelPosition.LEFT;
+    @Override
+    public Car buildFord(final FordModel model) {
+        switch(model){
+            case FOCUS:
+                return new Ford("2000", "Gasoline", "2018", position);
+            case MUSTANG:
+                return new Ford("3200", "Gasoline", "2020", position);
+            default:
+                throw new IllegalArgumentException("unknown model");
+        }
+    }
+
+    @Override
+    public Car buildAudi(final Audi model) {
+        return null;
+    }
+}
