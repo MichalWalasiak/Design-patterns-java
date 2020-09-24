f7747d66ed3538a17bb7d9ac4cbb639f6c6fd99d
Michał Walasiak <m.walasiak@gmail.com>
2020-09-24 22:10:54 +0200

    Override method

--- src/main/java/io/github/mwalasiak/Factory/training/CommonWheelFactory.java
+++ src/main/java/io/github/mwalasiak/Factory/training/CommonWheelFactory.java
@@ -5,7 +5,14 @@ public class CommonWheelFactory implements Factory {
     SteringWheelPosition position = SteringWheelPosition.RIGHT;
     @Override
     public Car buildFord(final FordModel model) {
-        return null;
+        switch (model){
+            case MUSTANG:
+                return new Ford("3200", "Gasoline", "2020", position);
+            case FOCUS:
+                return new Ford("2000", "Gasoline", "2018", position);
+            default:
+                throw new IllegalArgumentException("unknown model");
+        }
     }
 
     @Override
