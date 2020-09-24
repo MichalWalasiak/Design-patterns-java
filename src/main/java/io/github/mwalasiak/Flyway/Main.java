8c0f3ffffddb705c6979a8a049f613923838c37b
Michał Walasiak <m.walasiak@gmail.com>
2020-09-24 22:24:00 +0200

    CReate list of Objects

--- src/main/java/io/github/mwalasiak/Flyway/Main.java
+++ src/main/java/io/github/mwalasiak/Flyway/Main.java
@@ -1,8 +1,13 @@
 package io.github.mwalasiak.Flyway;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        List<Object> activeUnits = new ArrayList<>();
 
     }
 }
