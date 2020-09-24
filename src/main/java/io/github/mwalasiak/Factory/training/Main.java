a28d0aba942146132a182b55a311eef5b308b852
Michał Walasiak <m.walasiak@gmail.com>
2020-09-24 22:18:36 +0200

    implement sample main method

--- src/main/java/io/github/mwalasiak/Factory/training/Main.java
+++ src/main/java/io/github/mwalasiak/Factory/training/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Factory continentalFactory = new ContinentalFactory();
+        Factory commonWheelFactory = new CommonWheelFactory();
+
+        Car ford = continentalFactory.buildFord(FordModel.MUSTANG);
+        System.out.println(ford.getPosition());
+
+        Car audi = commonWheelFactory.buildAudi(AudiModel.SQ7);
+        System.out.println(audi.getPosition());
+
+
     }
 }
