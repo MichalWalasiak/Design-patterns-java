47b381ae36c81751bbde2ffbae5fefa0e2373cb4
Michał Walasiak <m.walasiak@gmail.com>
2020-09-25 22:00:44 +0200

    create private fields

--- src/main/java/io/github/mwalasiak/Flyway/TeslaTank.java
+++ src/main/java/io/github/mwalasiak/Flyway/TeslaTank.java
@@ -1,4 +1,13 @@
 package io.github.mwalasiak.Flyway;
 
 public class TeslaTank {
+
+    private String name;
+    private int hp;
+    private int armour;
+    private int damageDelt;
+    private int x;
+    private int y;
+    private int speed;
+    private int hpLeft;
 }
