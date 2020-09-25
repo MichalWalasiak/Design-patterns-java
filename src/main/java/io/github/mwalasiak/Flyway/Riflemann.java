98de2b87c22f57cbeea9fbcda6bad96cd6261df8
Michał Walasiak <m.walasiak@gmail.com>
2020-09-25 22:06:14 +0200

    corrections

--- src/main/java/io/github/mwalasiak/Flyway/Riflemann.java
+++ src/main/java/io/github/mwalasiak/Flyway/Riflemann.java
@@ -10,6 +10,7 @@ public class Riflemann {
     private int y;
     private int speed;
     private int hpLeft;
+    private int resourceCost;
 
     public Riflemann(final String name, final int hp, final int armour, final int damageDelt, final int x, final int y, final int speed) {
         this.name = name;
