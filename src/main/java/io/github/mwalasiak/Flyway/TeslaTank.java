88f892f15f1114b3017494d14d09c3fd7aaa8e1f
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 22:07:05 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/TeslaTank.java
+++ src/main/java/io/github/mwalasiak/Flyway/TeslaTank.java
@@ -2,25 +2,18 @@ package io.github.mwalasiak.Flyway;
 
 public class TeslaTank {
 
-    private String name;
-    private int hp;
-    private int armour;
-    private int damageDelt;
+
     private int x;
     private int y;
-    private int speed;
     private int hpLeft;
-    private int resourceCost;
+    private TeslaTankUnitStats teslastats;
 
-    public TeslaTank(final String name, final int hp, final int armour, final int damageDelt, final int x, final int y, final int speed, final int resourceCost) {
-        this.name = name;
-        this.hp = hp;
-        this.armour = armour;
-        this.damageDelt = damageDelt;
+
+    public TeslaTank(final int x, final int y) {
+        teslastats = UnitStatsRepository.
         this.x = x;
         this.y = y;
-        this.speed = speed;
-        this.hpLeft = hp;
-        this.resourceCost = resourceCost;
+        this.hpLeft = teslastats.getHp();
+
     }
 }
