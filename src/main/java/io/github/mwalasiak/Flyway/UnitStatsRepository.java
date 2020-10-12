d6bb538b1356a3caac273686df163e2da8c77108
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 22:09:30 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/UnitStatsRepository.java
+++ src/main/java/io/github/mwalasiak/Flyway/UnitStatsRepository.java
@@ -4,6 +4,7 @@ public class UnitStatsRepository {
 
     private static AircraftCarrierUnitStats carrierUnitStats = new AircraftCarrierUnitStats("Saratoga", 250, 400, 500, 50, 5000);
     private static TeslaTankUnitStats teslaTankUnitStats = new TeslaTankUnitStats("tesla1", 50, 25, 15, 15, 10);
+    private static RiflemannUnitStats riflemannUnitStats = new RiflemannUnitStats("rifle1", 50, 25, 15, 15, 10);
 
     public UnitStatsRepository() {
     }
