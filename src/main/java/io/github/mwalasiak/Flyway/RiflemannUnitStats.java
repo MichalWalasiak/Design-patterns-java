b19a01cc3087089fe28e0f327556b82e0b41aed6
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 22:09:22 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/RiflemannUnitStats.java
+++ src/main/java/io/github/mwalasiak/Flyway/RiflemannUnitStats.java
@@ -1,4 +1,44 @@
 package io.github.mwalasiak.Flyway;
 
 public class RiflemannUnitStats {
+
+    private String name;
+    private int speed;
+    private int resourceCost;
+    private int hp;
+    private int armour;
+    private int damageDelt;
+
+    public RiflemannUnitStats(final String name, final int speed, final int resourceCost, final int hp, final int armour, final int damageDelt) {
+        this.name = name;
+        this.speed = speed;
+        this.resourceCost = resourceCost;
+        this.hp = hp;
+        this.armour = armour;
+        this.damageDelt = damageDelt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getResourceCost() {
+        return resourceCost;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getArmour() {
+        return armour;
+    }
+
+    public int getDamageDelt() {
+        return damageDelt;
+    }
 }
