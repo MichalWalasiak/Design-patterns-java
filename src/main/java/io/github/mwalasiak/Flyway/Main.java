28e80569da7b4733b8d16ee853e029539d02086e
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 22:21:46 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/Main.java
+++ src/main/java/io/github/mwalasiak/Flyway/Main.java
@@ -10,9 +10,9 @@ public class Main {
         List<Object> activeUnits = new ArrayList<>();
 
         for (int i = 0; i < 1000; i++){
-            activeUnits.add(new TeslaTank("tesla1", 100, 50, 25, 40, 45, 150, 100));
-            activeUnits.add(new Riflemann("rifle1", 50, 25, 15, 15, 10, 10, 5));
-            activeUnits.add(new AircraftCarrier("Saratoga", 250, 400, 500, 50, 45, 45, 5000));
+            activeUnits.add(new TeslaTank(0,0));
+            activeUnits.add(new Riflemann( 0, 0));
+            activeUnits.add(new AircraftCarrier( 0, 0));
         }
     }
 }
