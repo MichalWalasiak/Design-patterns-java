914394c8eb5e8253ee9bbff6e4d48a1e908f6690
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 21:38:18 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/AircraftCarrier.java
+++ src/main/java/io/github/mwalasiak/Flyway/AircraftCarrier.java
@@ -2,15 +2,13 @@ package io.github.mwalasiak.Flyway;
 
 public class AircraftCarrier {
 
-    private String name;
-    private int hp;
-    private int armour;
-    private int damageDelt;
+
+
     private int x;
     private int y;
-    private int speed;
     private int hpLeft;
-    private int resourceCost;
+
+
 
     public AircraftCarrier(final String name, final int hp, final int armour, final int damageDelt, final int x, final int y, final int speed, final int resourceCost) {
         this.name = name;
