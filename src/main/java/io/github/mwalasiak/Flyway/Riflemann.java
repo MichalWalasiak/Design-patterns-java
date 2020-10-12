af7751a6aaeeba0e16ecf8a3b8e0e19294b5b728
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 22:11:23 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/Riflemann.java
+++ src/main/java/io/github/mwalasiak/Flyway/Riflemann.java
@@ -6,12 +6,14 @@ public class Riflemann {
     private int x;
     private int y;
     private int hpLeft;
+    private RiflemannUnitStats riflemannUnitStats;
 
 
     public Riflemann(final int x, final int y) {
+        riflemannUnitStats = UnitStatsRepository.
         this.x = x;
         this.y = y;
-        this.hpLeft = hp;
+        this.hpLeft = riflemannUnitStats.getHp();
 
     }
 }
