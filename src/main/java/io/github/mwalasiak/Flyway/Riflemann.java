cbb505b3fd759f580970eaea390997b98cd0f639
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 22:06:55 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/Riflemann.java
+++ src/main/java/io/github/mwalasiak/Flyway/Riflemann.java
@@ -2,25 +2,16 @@ package io.github.mwalasiak.Flyway;
 
 public class Riflemann {
 
-    private String name;
-    private int hp;
-    private int armour;
-    private int damageDelt;
+
     private int x;
     private int y;
-    private int speed;
     private int hpLeft;
-    private int resourceCost;
 
-    public Riflemann(final String name, final int hp, final int armour, final int damageDelt, final int x, final int y, final int speed, final int resourceCost) {
-        this.name = name;
-        this.hp = hp;
-        this.armour = armour;
-        this.damageDelt = damageDelt;
+
+    public Riflemann(final int x, final int y) {
         this.x = x;
         this.y = y;
-        this.speed = speed;
         this.hpLeft = hp;
-        this.resourceCost = resourceCost;
+
     }
 }
