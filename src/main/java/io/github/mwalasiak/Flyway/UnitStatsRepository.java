339a91815db87173a576d05d8f2c61dc4aaabb00
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 21:50:44 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/UnitStatsRepository.java
+++ src/main/java/io/github/mwalasiak/Flyway/UnitStatsRepository.java
@@ -1,4 +1,13 @@
 package io.github.mwalasiak.Flyway;
 
 public class UnitStatsRepository {
+
+    private static AircraftCarrierUnitStats carrierUnitStats = new AircraftCarrierUnitStats("Saratoga", 250, 400, 500, 50, 5000);
+
+    public UnitStatsRepository() {
+    }
+
+    public AircraftCarrierUnitStats getCarrierUnitStats() {
+        return carrierUnitStats;
+    }
 }
