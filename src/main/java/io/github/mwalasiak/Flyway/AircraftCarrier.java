0eef77dc0daf69f08e2a0fc2f9dca95401bb8736
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 21:42:46 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/AircraftCarrier.java
+++ src/main/java/io/github/mwalasiak/Flyway/AircraftCarrier.java
@@ -2,23 +2,16 @@ package io.github.mwalasiak.Flyway;
 
 public class AircraftCarrier {
 
-
-
     private int x;
     private int y;
     private int hpLeft;
+    private AircraftCarrierUnitStats carrierUnitStats;
 
+    public AircraftCarrier(final int x, final int y, final AircraftCarrierUnitStats carrierUnitStats) {
 
-
-    public AircraftCarrier(final String name, final int hp, final int armour, final int damageDelt, final int x, final int y, final int speed, final int resourceCost) {
-        this.name = name;
-        this.hp = hp;
-        this.armour = armour;
-        this.damageDelt = damageDelt;
         this.x = x;
         this.y = y;
-        this.speed = speed;
-        this.hpLeft = hp;
-        this.resourceCost = resourceCost;
+        this.carrierUnitStats = carrierUnitStats;
+        this.hpLeft = carrierUnitStats.getHp();
     }
 }
