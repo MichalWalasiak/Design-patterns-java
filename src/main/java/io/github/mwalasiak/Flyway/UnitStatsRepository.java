5ed600601b284f30f9d4a8564282436e6e040b71
Michał Walasiak <m.walasiak@gmail.com>
2020-10-12 22:21:36 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/UnitStatsRepository.java
+++ src/main/java/io/github/mwalasiak/Flyway/UnitStatsRepository.java
@@ -2,22 +2,22 @@ package io.github.mwalasiak.Flyway;
 
 public class UnitStatsRepository {
 
-    private static AircraftCarrierUnitStats carrierUnitStats = new AircraftCarrierUnitStats("Saratoga", 250, 400, 500, 50, 5000);
-    private static TeslaTankUnitStats teslaTankUnitStats = new TeslaTankUnitStats("tesla1", 50, 25, 15, 15, 10);
-    private static RiflemannUnitStats riflemannUnitStats = new RiflemannUnitStats("rifle1", 50, 25, 15, 15, 10);
+    private static UnitStats carrierUnitStats = new UnitStats("Saratoga", 250, 400, 500, 50, 5000);
+    private static UnitStats unitStats = new UnitStats("tesla1", 50, 25, 15, 15, 10);
+    private static UnitStats riflemannUnitStats = new UnitStats("rifle1", 50, 25, 15, 15, 10);
 
     public UnitStatsRepository() {
     }
 
-    public AircraftCarrierUnitStats getCarrierUnitStats() {
+    public static UnitStats getCarrierUnitStats() {
         return carrierUnitStats;
     }
 
-    public TeslaTankUnitStats getTeslaTankUnitStats() {
-        return teslaTankUnitStats;
+    public static UnitStats getTeslaTankUnitStats() {
+        return unitStats;
     }
 
-    public RiflemannUnitStats getRiflemannUnitStats() {
+    public static UnitStats getRiflemannUnitStats() {
         return riflemannUnitStats;
     }
 }
