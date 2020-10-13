bf5bae9c0d1480afae1806870de3de4b7c83b453
Michał Walasiak <m.walasiak@gmail.com>
2020-10-13 20:24:10 +0200

    updates

--- src/main/java/io/github/mwalasiak/Flyway/training/WhitePiece.java
+++ src/main/java/io/github/mwalasiak/Flyway/training/WhitePiece.java
@@ -3,6 +3,6 @@ package io.github.mwalasiak.Flyway.training;
 public class WhitePiece extends ChessPiece {
 
     public WhitePiece() {
-        super("Biały pionek", "2", "a", "biały");
+        super("Biały pionek", "2", "a", "white");
     }
 }
