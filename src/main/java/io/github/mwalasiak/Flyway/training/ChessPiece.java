16c86a15098521d8238452d6ed317dcacf96e603
Michał Walasiak <m.walasiak@gmail.com>
2020-10-13 20:04:03 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/training/ChessPiece.java
+++ src/main/java/io/github/mwalasiak/Flyway/training/ChessPiece.java
@@ -5,7 +5,20 @@ public class ChessPiece {
     private String name;
     private String numberPosition;
     private String letterPosition;
-    private String color;
+    private Color color;
 
+    public ChessPiece(final String name, final String numberPosition, final String letterPosition, final String color) {
+        this.name = name;
+        this.numberPosition = numberPosition;
+        this.letterPosition = letterPosition;
 
+        switch (color.toLowerCase()) {
+            case "black":
+                this.color = ColorRepository.getBlack();
+                break;
+            default:
+                this.color = ColorRepository.getWhite();
+                break;
+        }
+    }
 }
