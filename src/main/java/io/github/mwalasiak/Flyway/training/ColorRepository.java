1c85f9a4ca96336ca3cd00eb1ad62173bda2e5fb
Michał Walasiak <m.walasiak@gmail.com>
2020-10-13 20:04:10 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/training/ColorRepository.java
+++ src/main/java/io/github/mwalasiak/Flyway/training/ColorRepository.java
@@ -2,14 +2,14 @@ package io.github.mwalasiak.Flyway.training;
 
 public class ColorRepository {
 
-    private static Color white = new Color(0,0,0);
-    private static Color black = new Color(255,255,255);
+    static Color white = new Color(0,0,0);
+    static Color black = new Color(255,255,255);
 
-    private static Color getWhite() {
+    public static Color getWhite() {
         return white;
     }
 
-    private static Color getBlack () {
+    public static Color getBlack () {
         return black;
     }
 }
