8695e4f818d62a70b82d53320f07ef5b78cda8be
Michał Walasiak <m.walasiak@gmail.com>
2020-10-13 20:40:22 +0200

    sample implementation

--- src/main/java/io/github/mwalasiak/Flyway/training/Main.java
+++ src/main/java/io/github/mwalasiak/Flyway/training/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ChessPiece blackPawn = new BlackPiece("Czarny Pionek", "1", "a");
+        ChessPiece whitePawn = new WhitePiece("Biały Pionek", "1", "a");
+        ChessPiece whiteQueen = new WhiteQueen("Biała Królowa");
+        ChessPiece blackQueen = new BlackQueen("Czarna Kólowa");
+
+        System.out.println(blackPawn.getColor() == blackQueen.getColor());
+        System.out.println(whitePawn.getColor() == whiteQueen.getColor());
+
     }
 }
