a24de2e3909b7d1f66a445f7eb59f288dfc5732d
Michał Walasiak <m.walasiak@gmail.com>
2020-10-13 19:55:13 +0200

    add fields

--- src/main/java/io/github/mwalasiak/Flyway/training/ChessPiece.java
+++ src/main/java/io/github/mwalasiak/Flyway/training/ChessPiece.java
@@ -1,4 +1,11 @@
 package io.github.mwalasiak.Flyway.training;
 
 public class ChessPiece {
+
+    private String name;
+    private String numberPosition;
+    private String letterPosition;
+    private String color;
+
+
 }
