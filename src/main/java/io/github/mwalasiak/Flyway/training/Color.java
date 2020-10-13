ec9df9b0001c8627347387dd29b69a28330ee21a
Michał Walasiak <m.walasiak@gmail.com>
2020-10-13 19:46:09 +0200

    work in progress

--- src/main/java/io/github/mwalasiak/Flyway/training/Color.java
+++ src/main/java/io/github/mwalasiak/Flyway/training/Color.java
@@ -1,4 +1,14 @@
 package io.github.mwalasiak.Flyway.training;
 
 public class Color {
+
+    private int r;
+    private int g;
+    private int b;
+
+    public Color(final int r, final int g, final int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
 }
